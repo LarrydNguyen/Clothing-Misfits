2273a437d1bb4e6826eeedbf813b9c9e722647db
Larry Nguyen <larrynguyen771@gmail.com>
2022-10-07 20:39:54 -0500

    Added new nav bar, colors and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,32 @@
 module.exports = {
   content: [
     './views/*.ejs',
-    './vies/partials/*.ejs'
+    './views/partials/*.ejs'
   ],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    
     extend: {},
   },
+  //daisyUI plugin
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+  daisyui:{
+    themes: ["garden"]
+  },
+  
 }
