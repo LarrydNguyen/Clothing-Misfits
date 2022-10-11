65625535131e0973ec47c8e7a035b1348e984033
Larry Nguyen <larrynguyen771@gmail.com>
2022-10-10 20:46:43 -0500

    edit feed images and added likes to feed page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,19 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    
-    extend: {},
+    extend: {
+    colors: {
+        primary: "#5c7f67",
+        secondary: "#ecf4e7",
+        "secondary-content": "#24331a",
+        accent: "#fae5e5",
+        "accent-content": "#322020",
+        neutral: "#5d5656",
+        "neutral-content": "#e9e7e7",
+        "base-100": "#e9e7e7",
+        "base-content": "#100f0f", 
+      },
+    },
   },
   //daisyUI plugin
   plugins: [require("daisyui")],
